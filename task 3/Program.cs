d21bb417ae5db8efa26d8c500b884ebc23f114a9
utkrist00-shahi <utkristshahi56@gmail.com>
2025-11-13 10:25:14 +0545

    task 3 completed

---
+++ task 3/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.Write("Enter your age: ");
+            string age_input = Console.ReadLine();
+
+            int age = Convert.ToInt32(age_input);
+
+            if (age < 13)
+            {
+                Console.WriteLine("Child");
+            }
+            else if (age >= 13 && age < 20)
+            {
+                Console.WriteLine("Teenager");
+            }
+            else
+            {
+                Console.WriteLine("Adult");
+            }
+        }
+    }
+}
