5fb2866c54b4744e6fb0e3b7ff956ccc6b20e996
utkrist00-shahi <utkristshahi56@gmail.com>
2025-11-13 10:26:41 +0545

    task 4 completed

---
+++ task 4/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.Write("Enter a number (1-7): ");
+            string number_input = Console.ReadLine();
+            int number = Convert.ToInt32(number_input);
+
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("Sunday");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Monday");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Tuesday");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Wednesday");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Thursday");
+                    break;
+
+                case 6:
+                    Console.WriteLine("Friday");
+                    break;
+
+                case 7:
+                    Console.WriteLine("Saturday");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid day number");
+                    break;
+            }
+        }
+    }
+}
