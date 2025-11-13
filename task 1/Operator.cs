8dc674b86fa45f2a5b54dae72b80e09444295b06
utkrist00-shahi <utkristshahi56@gmail.com>
2025-11-13 10:20:32 +0545

    task 1 complete

---
+++ task 1/Operator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public class Operator
+    {
+
+
+        public static void Add(int a, int b)
+        {
+            Console.WriteLine($"Sum of two numbers: {a + b}");
+        }
+
+        public static void Subtract(int a, int b)
+        {
+            Console.WriteLine($"Difference of two numbers: {a - b}");
+        }
+
+        public static void Multiply(int a, int b)
+        {
+            Console.WriteLine($"Product of two numbers: {a * b}");
+        }
+
+        public static void Divide(int a, int b)
+        {
+            Console.WriteLine($"The result of division of two numbers: {a / b}");
+        }
+
+        public static void OddEvenFinder(int number)
+        {
+            string value = (number % 2 == 0) ? $"{number} is Even Number" : $"{number} is Odd Number";
+            Console.WriteLine($"{value}");
+        }
+    }
+}
---
+++ task 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+
+namespace Task_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Operator.Add(20, 5);
+            Operator.Subtract(10, 5);
+            Operator.Multiply(30, 5);
+            Operator.Divide(10, 5);
+            Operator.OddEvenFinder(9);
+            Operator.OddEvenFinder(7);
+
+        }
+
+
+    }
+}
